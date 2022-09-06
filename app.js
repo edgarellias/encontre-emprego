9a673a9acb93774d1f8d17c0adf51fe4068de82c
Edgar Elias <edgar_jk3@hotmail.com>
2022-09-06 16:43:36 -0300

    estrutura html pronta

--- app.js
+++ app.js
@@ -1,25 +1,30 @@
 const express = require("express");
 const app = express();
 const db = require("./db/connection");
+const bodyParser = require("body-parser");
 
 const PORT = 3000;
 
 app.listen(PORT, function () {
 	console.log(`O express está rodando na porta ${PORT}`);
 });
+// body parser
 
+app.use(bodyParser.urlencoded({ extended: false }));
 // db connection
 
-db
-.authenticate()
-.then(() => {
-	console.log("Conectou ao banco com sucesso");
-})
-.catch(err => {
-    console.log("Ocorreu um erro ao conectar", err)
-})
+db.authenticate()
+	.then(() => {
+		console.log("Conectou ao banco com sucesso");
+	})
+	.catch((err) => {
+		console.log("Ocorreu um erro ao conectar", err);
+	});
 
 // routes
 app.get("/", (req, res) => {
 	res.send("Está funcionando111");
 });
+
+// jobs routes
+app.use("/jobs", require("./routes/jobs"))
---
+++ routes/jobs.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const router = express.Router();
+const Job = require("../models/Job.js");
+
+router.get("/test", (req, res) => {
+    res.send("deu certo");
+})
+// add job via post
+router.post("/add", (req, res) => {
+
+    let {title, salary, company, description, email, new_job} = req.body;
+
+    // insert
+    Job.create({
+        title,
+        description,
+        salary,
+        company,
+        email,
+        new_job
+    })
+    .then(() => res.redirect("/"))
+    .catch(err => console.log(err))
+})
+
+module.exports = router
